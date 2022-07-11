9795e4bb717af09037f7ac3fde86bcb000cc644f
UrbanCoffee <rudy.m.7854@gmail.com>
2022-07-11 13:05:52 -0700

    Progress in Create: Reponsive preview

--- urbancoffee/src/components/Create-Components/inputTEXTAREA.js
+++ urbancoffee/src/components/Create-Components/inputTEXTAREA.js
@@ -1,13 +1,18 @@
 import './styling/inputTEXTAREA.css'
 
-const InputTEXTAREA = () => {
+const InputTEXTAREA = ({ updateBody }) => {
     //{/* Look into useLayoutEffect */}
+
+    const parse = input => {
+        updateBody(input.target.value);
+    }
     return (
         <textarea placeholder='Write Document Body Here...'
             id='body'
             name='body'
             required
 
+            onInput={parse}
             className="Create-TA"
             />
     )
--- urbancoffee/src/routes/Create.js
+++ urbancoffee/src/routes/Create.js
@@ -1,16 +1,29 @@
 import '../components/styling/buttons.css'
 import './styling/Create.css'
-import { NavBar }    from '../components'
+import { useState } from 'react'
+import { NavBar, DocBanner, Document}    from '../components'
 import { InputFILE, InputTEXTAREA } from '../components/Create-Components'
 
 const Create = () => {
+    const [ docTitle, setTitle ] = useState("");
+    const [ docSubtitle, setSubtitle ] = useState("");
+    const [ docBody, setBody ] = useState([]);
+
+    const updTitle  = input => setTitle(input.target.value);
+    const updSubTtl = input => setSubtitle(input.target.value);
+    // Have updBody here in the mean time before a proper
+    // parse function can be implemented in <inputTEXTAREA/>
+    const updBody   = value => {console.log(value);};
 
     return (
         <>
         <NavBar/>
         <div className='Container' id='Create'>
             <header>
-                <div><span>Create a document -</span> Untitled</div>
+                {/* Fix when screen is small and title is too long */}
+                <div><span>Create a document - </span> 
+                    {docTitle.length? docTitle : "Untitled"}
+                </div>
                 <button 
                     className='btn-Blue btn-large'
                     onClick={() => alert("Published Press.")}>
@@ -18,23 +31,42 @@ const Create = () => {
                 </button>
             </header>
 
-            <form method="POST">
-                <InputFILE /><br/>
+            <div className='Create-Body'>
+                <form method="POST">
+                    <InputFILE /><br/>
 
-                <input type='text' placeholder='Title'
-                    id='title'
-                    name='title'
-                    maxLength={'50'}
-                    required
-                    /><br/>
+                    <input type='text' placeholder='Title'
+                        id='title'
+                        name='title'
+                        maxLength={'50'}
+                        onInput={updTitle}
+                        required
+                        /><br/>
 
-                <input type='text' placeholder='Subtitle'
-                    id='subtitle'
-                    name='subtitle'
-                    /><br/>
+                    <input type='text' placeholder='Subtitle'
+                        id='subtitle'
+                        name='subtitle'
+                        onInput={updSubTtl}
+                        /><br/>
 
-                <InputTEXTAREA />
-            </form>
+                    <InputTEXTAREA 
+                        updateBody={updBody}/>
+                </form>
+
+                <div id='Preview'>
+                    <div className='DocPH'>
+                        <DocBanner/>
+                        <Document
+                            doc={{
+                                title: docTitle,
+                                subtitle: docSubtitle,
+                                author: "Placeholder",
+                                body: docBody
+                            }}
+                            />
+                    </div>
+                </div>
+            </div>
         </div>
         </>
     )
